60d15a9db4442ae9e76aaed88c5c15d900f9467d
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-03-29 18:30:07 +0200

    cssfix

--- src/components/app/Header.js
+++ src/components/app/Header.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native'
 import { tw } from '~/src/libs/tailwind'
 
 const Header = ({ title }) => {
-  return <View style={tw('flex items-center align-middle p-5')}>
+  return <View style={tw('flex items-center p-5')}>
     <Text>{title}</Text>
   </View>
 }
