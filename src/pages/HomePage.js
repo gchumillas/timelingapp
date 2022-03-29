9c88e9c5ce735048aec37faa3543cab2ce9d904a
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-03-29 22:24:45 +0200

    styling flatlist

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,21 @@
 import React from 'react'
-import { Text, SectionList } from 'react-native'
+import { Text, View, FlatList } from 'react-native'
 import { getEventsGroupedByDate } from '~/src/providers/events'
 import PageLayout from '~/src/layouts/PageLayout'
 import DateSelector from '~/src/components/inputs/DateSelector'
+import { tw } from '../libs/tailwind'
+
+const SectionItem = ({ events }) => {
+  return <FlatList
+    data={events}
+    renderItem={({ item, index }) => <View style={{ ...tw('flex-1 p-1'), maxWidth: '50%' }}>
+      <View style={tw('border')}>
+        <Text key={item.id}>{item.userName}</Text>
+      </View>
+    </View>}
+    keyExtractor={item => item.id}
+    numColumns={2} />
+}
 
 const HomePage = () => {
   const [dateFilter, setDateFilter] = React.useState(null) // DateTime
@@ -10,10 +23,7 @@ const HomePage = () => {
 
   const loadEvents = async _ => {
     const events = await getEventsGroupedByDate()
-    // rename `events` by `data`, as the SectionList component requires a `data` property
-    const items = events.map(({ events, ...item }) => ({ data: events, ...item }))
-
-    setEventsGroupedByDate(items)
+    setEventsGroupedByDate(events)
   }
 
   React.useEffect(_ => {
@@ -22,11 +32,13 @@ const HomePage = () => {
 
   return <PageLayout title="Nearby events">
     <DateSelector numDays={7} value={dateFilter} onChange={setDateFilter} />
-    <SectionList
-      sections={eventsGroupedByDate}
-      renderSectionHeader={({ section }) => <Text>{section.date}</Text>}
-      renderItem={({ item }) => <Text>{item.userName}</Text>}
-      keyExtractor={item => item.id}
+    <FlatList
+      data={eventsGroupedByDate}
+      renderItem={({ item }) => <View>
+        <Text key={item.date}>{item.date}</Text>
+        <SectionItem events={item.events} />
+      </View>}
+      keyExtractor={item => item.date}
     />
   </PageLayout>
 }
