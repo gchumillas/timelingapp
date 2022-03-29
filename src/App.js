9f9934776d3e12b5355fdea043d1166904e03ddb
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-03-29 17:12:14 +0200

    use absolute paths

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NativeRouter, Routes, Route } from 'react-router-native'
-import HomePage from './pages/HomePage'
+import HomePage from '~/src/pages/HomePage'
 
 export default function App () {
   return <NativeRouter>
