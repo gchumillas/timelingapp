e7e1a2fda30295d46fba216500f99d65c03b174e
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-03-30 16:21:40 +0200

    css update

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -8,8 +8,8 @@ import PlaceIcon from '~/assets/icons/place.svg'
 const Thumbnail = ({ id, title, userName, location, image }) => {
   return <View style={{ ...tw('w-1/2 pb-4 px-2') }}>
     <Link to={`/events/${id}`}>
-      <View style={{ ...tw('relative border rounded-lg overflow-hidden'), aspectRatio: 4 / 5 }}>
-        <Image source={images[image]} style={{ ...tw('absolute w-full h-full rounded-lg') }} resizeMode="cover" />
+      <View style={{ ...tw('relative border-1/2 border-gray-400 rounded-lg overflow-hidden'), aspectRatio: 4 / 5 }}>
+        <Image source={images[image]} style={{ ...tw('absolute w-full h-full rounded-lg border-2'), borderColor: 'white' }} resizeMode="cover" />
         <View style={tw('absolute bottom-2 left-2')}>
           <Text style={tw('text-white text-lg font-bold')}>{title}</Text>
           <Text style={tw('text-white mb-0.5')}>With {userName}</Text>
