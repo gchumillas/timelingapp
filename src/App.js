559a13d88c91f3b3a488a8bd6362492f1d2b8302
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-03-30 06:19:02 +0200

    add inline to-dos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { NativeRouter, Routes, Route } from 'react-router-native'
 import HomePage from '~/src/pages/HomePage'
 
+// TODO: add redux
+// TODO: add comments
 export default function App () {
   return <NativeRouter>
     <Routes>
