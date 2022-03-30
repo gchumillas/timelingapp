40871b902bf91d32767bd4bc65400a03aa76f966
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-03-30 17:45:11 +0200

    css updates

--- src/components/app/Header.js
+++ src/components/app/Header.js
@@ -4,9 +4,9 @@ import { tw, getColor } from '~/src/libs/tailwind'
 import SettingsIcon from '~/assets/icons/settings.svg'
 
 const Header = ({ title }) => {
-  return <View style={tw('relative flex items-center p-5')}>
+  return <View style={tw('relative flex items-center m-5 mt-0')}>
     <Text style={tw('text-lg font-semibold')}>{title}</Text>
-    <Pressable style={{ ...tw('absolute right-5 inset-y-0 justify-center') }}>
+    <Pressable style={{ ...tw('absolute right-0 inset-y-0 justify-center') }}>
       <SettingsIcon width={20} height={20} color={getColor('gray-900')} />
     </Pressable>
   </View>
