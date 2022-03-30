0f551629601b175a0eb4a4e6d69a3b4f032bf08d
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-03-30 16:37:10 +0200

    css update

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -11,10 +11,10 @@ const Thumbnail = ({ id, title, userName, location, image }) => {
     <Link to={`/events/${id}`}>
       <View style={{ ...tw('relative border-1/2 border-gray-400 rounded-lg overflow-hidden'), aspectRatio: 4 / 5 }}>
         <Image source={images[image]} style={{ ...tw('absolute w-full h-full rounded-lg border-2'), borderColor: 'white' }} resizeMode="cover" />
-        <Pressable style={tw('absolute top-2 right-2')}>
+        <Pressable style={tw('absolute top-3 right-3')}>
           <HeartIcon with={18} height={18} color="white" />
         </Pressable>
-        <View style={tw('absolute bottom-2 left-2')}>
+        <View style={tw('absolute bottom-3 left-3')}>
           <Text style={tw('text-white text-lg font-bold')}>{title}</Text>
           <Text style={tw('text-white mb-0.5')}>With {userName}</Text>
           <View style={tw('flex flex-row items-center -ml-0.5')}>
