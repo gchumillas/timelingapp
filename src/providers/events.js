da1c0f6dd9261b27af0f079f30fbc37fd9649b31
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-03-30 19:44:27 +0200

    add getEvent provider

--- src/providers/events.js
+++ src/providers/events.js
@@ -4,3 +4,10 @@ import events from '~/data/events.json'
 export const getEventsGroupedByDate = () => {
   return Promise.resolve(events.map(({ date, ...rest }) => ({ date: DateTime.fromISO(date), ...rest })))
 }
+
+export const getEvent = async id => {
+  const events = await getEventsGroupedByDate()
+  const items = events.flatMap(x => x.events)
+
+  return items.find(x => x.id == id)
+}
