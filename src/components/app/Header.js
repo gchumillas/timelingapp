f57a3ec2577f70cc17753e64f320e58906633ef1
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-03-30 17:02:59 +0200

    cssfix

--- src/components/app/Header.js
+++ src/components/app/Header.js
@@ -6,7 +6,7 @@ import SettingsIcon from '~/assets/icons/settings.svg'
 const Header = ({ title }) => {
   return <View style={tw('relative flex items-center p-5')}>
     <Text style={tw('text-lg font-semibold')}>{title}</Text>
-    <Pressable style={{ ...tw('absolute right-5 top-6') }}>
+    <Pressable style={{ ...tw('absolute right-5 inset-y-0 justify-center') }}>
       <SettingsIcon width={20} height={20} color={getColor('gray-900')} />
     </Pressable>
   </View>
