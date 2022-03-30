53bdab415d311c6e3bbeeab3b8bdadeae34fecd2
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-03-30 19:44:35 +0200

    load event

--- src/pages/EventPage.js
+++ src/pages/EventPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { SafeAreaView, View, Image, Text, Pressable } from 'react-native'
 import { useNavigate, useParams } from 'react-router-native'
 import { tw } from '~/src/libs/tailwind'
+import { getEvent } from '~/src/providers/events'
 import ModalLayout from '~/src/layouts/ModalLayout'
 import LeftNavIcon from '~/assets/icons/left-nav.svg'
 import EllispsisIcon from '~/assets/icons/ellipsis.svg'
@@ -10,12 +11,22 @@ import images from '~/data/images'
 const EventPage = _ => {
   const navigate = useNavigate()
   const { id } = useParams()
+  const [event, setEvent] = React.useState()
   const doClose = _ => navigate('/events')
 
+  const loadEvent = async _ => {
+    const event = await getEvent(id)
+    setEvent(event)
+  }
+
+  React.useEffect(_ => {
+    loadEvent()
+  }, [id])
+
   return <ModalLayout>
-    <View style={{ ...tw('relative'), aspectRatio: 4 / 3 }}>
+    <View style={{ ...tw('relative bg-gray-400'), aspectRatio: 4 / 3 }}>
       {/* background image */}
-      <Image source={images.picture4} style={{ ...tw('absolute w-full h-full'), borderColor: 'white' }} resizeMode="cover" />
+      {event && <Image source={images[event.image]} style={{ ...tw('absolute w-full h-full'), borderColor: 'white' }} resizeMode="cover" />}
       {/* rest of elements */}
       <SafeAreaView style={tw('h-full')}>
         <View style={tw('h-full flex justify-between pt-3')}>
@@ -29,7 +40,7 @@ const EventPage = _ => {
           </View>
           <View style={tw('flex flex-row items-center justify-between py-4 px-6')}>
             <Text style={tw('text-xl text-white font-semibold')}>
-              Lucía 23
+              {event?.userName} {event?.age}
             </Text>
             <Text style={tw('text-base text-white underline')}>
               View Profile
