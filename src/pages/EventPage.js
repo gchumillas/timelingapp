c169a7df4cce2d66e68980b6fa70648d6a43e805
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-03-30 21:11:35 +0200

    update EventPage

--- src/pages/EventPage.js
+++ src/pages/EventPage.js
@@ -26,38 +26,43 @@ const EventPage = _ => {
   }, [id])
 
   return <ModalLayout>
-    <View style={{ ...tw('relative bg-gray-400'), aspectRatio: 4 / 3 }}>
-      {/* background image */}
-      {event && <Image source={images[event.image]} style={{ ...tw('absolute w-full h-full'), borderColor: 'white' }} resizeMode="cover" />}
-      {/* rest of elements */}
-      <SafeAreaView style={tw('h-full')}>
-        <View style={tw('h-full flex justify-between pt-3')}>
-          <View style={tw('mx-3 flex flex-row items-center justify-between')}>
-            <Pressable onPress={doClose}>
-              <LeftNavIcon width={30} height={30} color="white" />
-            </Pressable>
-            <Pressable>
-              <EllispsisIcon width={30} height={30} color="white" />
-            </Pressable>
+    <View style={tw('flex h-full')}>
+      <View style={{ ...tw('relative bg-gray-400'), aspectRatio: 4 / 3 }}>
+        {/* background image */}
+        {event && <Image source={images[event.image]} style={{ ...tw('absolute w-full h-full'), borderColor: 'white' }} resizeMode="cover" />}
+        {/* rest of elements */}
+        <SafeAreaView style={tw('h-full')}>
+          <View style={tw('h-full flex justify-between pt-3')}>
+            <View style={tw('mx-3 flex flex-row items-center justify-between')}>
+              <Pressable onPress={doClose}>
+                <LeftNavIcon width={30} height={30} color="white" />
+              </Pressable>
+              <Pressable>
+                <EllispsisIcon width={30} height={30} color="white" />
+              </Pressable>
+            </View>
+            <View style={tw('flex flex-row items-center justify-between py-4 px-6')}>
+              <Text style={tw('text-xl text-white font-semibold')}>
+                {event?.userName} {event?.age}
+              </Text>
+              <Text style={tw('text-base text-white underline')}>
+                View Profile
+              </Text>
+            </View>
           </View>
-          <View style={tw('flex flex-row items-center justify-between py-4 px-6')}>
-            <Text style={tw('text-xl text-white font-semibold')}>
-              {event?.userName} {event?.age}
-            </Text>
-            <Text style={tw('text-base text-white underline')}>
-              View Profile
-            </Text>
-          </View>
+        </SafeAreaView>
+      </View>
+      <View style={tw('bg-red-100 flex items-center py-7 px-7')}>
+        <Text style={tw('text-3xl font-semibold mb-3')}>{event?.title}</Text>
+        <Text style={tw('text-xl font-semibold mb-4')}>{event?.date.toLocaleString(DateTime.DATETIME_MED)}</Text>
+        <Text style={tw('mb-4 text-center')}>{event?.description}</Text>
+        <View style={tw('flex flex-row')}>
+          <PlaceIcon height={18} color={getColor('gray-900')} />
+          <Text style={tw('text-center')}>{event?.location}</Text>
         </View>
-      </SafeAreaView>
-    </View>
-    <View style={tw('bg-red-100 flex items-center py-7 px-7')}>
-      <Text style={tw('text-3xl font-semibold mb-3')}>{event?.title}</Text>
-      <Text style={tw('text-xl font-semibold mb-4')}>{event?.date.toLocaleString(DateTime.DATETIME_MED)}</Text>
-      <Text style={tw('mb-4 text-center')}>{event?.description}</Text>
-      <View style={tw('flex flex-row')}>
-        <PlaceIcon height={18} color={getColor('gray-900')} />
-        <Text style={tw('text-center')}>{event?.location}</Text>
+      </View>
+      <View style={tw('relative flex-grow')}>
+        <Image source={images.map} style={{ ...tw('absolute w-full h-full'), borderColor: 'white' }} resizeMode="cover" />
       </View>
     </View>
   </ModalLayout>
